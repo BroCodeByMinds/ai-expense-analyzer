d9088538a614aca05288fd8a8c90a9ce18c426f8
Gopal Patil <gpatil@paxiasolutions.com>
2026-05-20 10:50:34 +0530

    [EA-DEV-V1] Implemented document chunking service for ingestion pipeline

---
+++ app/ai/chunking/chunk_service.py
@@ -0,0 +1,37 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing import List
+from langchain_core.documents import Document
+from app.core.logging import setup_logger
+
+logger = setup_logger(__name__)
+
+class ChunkService:
+    def __init__(self, chunk_size: int = 1000, chunk_overlap : int = 200):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size,
+                                                            chunk_overlap=self.chunk_overlap)
+        
+    
+    async def chunk_documents(self, documents: List[Document]) -> List[Document]:
+        logger.info(
+            f"{self.__class__.__name__} | "
+            f"Starting document chunking. "
+            f"Total documents: {len(documents)}"
+        )
+
+        chunked_documents = self.text_splitter.split_documents(documents=documents)
+
+        for index, chunk in enumerate(chunked_documents):
+            chunk.metadata["chunk_index"] = index 
+        
+        logger.info(
+            f"{self.__class__.__name__} | "
+            f"Starting document chunking. "
+            f"Total documents: {len(documents)}"
+        )
+
+        return chunked_documents
+            
+
--- app/services/injestion_service.py
+++ app/services/injestion_service.py
@@ -1,14 +1,19 @@
-from app.ai.parsers.parser_factory import ParserFactory
-from app.core.logging import setup_logger
-from app.utils.file_utils import get_file_extension
+from typing import List
+from langchain_core.documents import Document
 
+from app.ai.chunking.chunk_service import ChunkService
+from app.ai.parsers.base_parser import BaseParser
+from app.ai.parsers.parser_factory import ParserFactory
+
+from app.utils.file_utils import get_file_extension
+from app.core.logging import setup_logger
 
 logger = setup_logger(__name__)
 
 
 class IngestionService:
 
-    async def parse_file(self, file_path: str):
+    async def parse_file(self, file_path: str) -> List[Document]:
 
         file_type : str = get_file_extension(file_path)
 
@@ -19,7 +24,7 @@ class IngestionService:
             f"File type: {file_type}"
         )
 
-        parser = ParserFactory.get_parser(file_type=file_type)
+        parser : BaseParser = ParserFactory.get_parser(file_type=file_type)
 
         logger.info(
             f"{self.__class__.__name__} | "
@@ -27,7 +32,7 @@ class IngestionService:
             f"Parser: {parser.__class__.__name__}"
         )
 
-        documents = await parser.parse(file_path=file_path)
+        documents : List[Document] = await parser.parse(file_path=file_path)
 
         logger.info(
             f"{self.__class__.__name__} | "
@@ -36,4 +41,15 @@ class IngestionService:
             f"{len(documents)}"
         )
 
-        return documents
+        chunk_service : ChunkService = ChunkService()
+
+        chunked_docs : List[Document] = await chunk_service.chunk_documents(documents=documents)
+
+        logger.info(
+            f"{self.__class__.__name__} | "
+            f"Chunking completed. "
+            f"Total chunks: "
+            f"{len(chunked_docs)}"
+        )
+
+        return chunked_docs
